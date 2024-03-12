b514abfd0a74a1ae1c12588324f0848b6c56d989
Gabriel <bielcheeeeee@gmail.com>
2024-03-12 12:44:33 -0300

    fixed users migrations table

--- migrations/20240311143337_users.sql
+++ migrations/20240311143337_users.sql
@@ -1,12 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS users (
-    user_id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    age VARCHAR(255) NOT NULL,
-    gender VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL
-);
+ALTER TABLE users
+ADD role varchar(255) NOT NULL;
 -- +goose StatementEnd
 
 -- +goose Down
